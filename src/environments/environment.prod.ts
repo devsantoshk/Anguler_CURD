52f19990767f02e35792fcb1da3814d5b12f9050
EDS_BMS <sk4809656@gmail.com>
2023-03-15 13:01:17 +0530

    Angular CRUD

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseconfig:{
+    apiKey: "Give your API Key from Firebase",
+    authDomain: "Give your",
+    databaseURL: "Give your",
+    projectId: "Give your ",
+    storageBucket: "Give your ",
+    messagingSenderId: "Give your"
+  }
+};
